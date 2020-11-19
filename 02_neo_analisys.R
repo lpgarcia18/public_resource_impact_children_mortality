3e47482e9c9585f2e01013c73c9fa7fe41fd6163
Leandro Pereira Garcia <lpgarcia18@gmail.com>
2020-11-19 20:53:05 -0300

    adjusting correlations

--- 02_neo_analisys.R
+++ 02_neo_analisys.R
@@ -19,198 +19,181 @@ library(survey)
 
 # Bases -----------------------------------------------------------------
 dose_resp <- read_csv("bases/completed_base.csv")
-dose_resp <- subset(dose_resp, dose_resp$LOCATION != "Vanuatu")
+#dose_resp <- subset(dose_resp, dose_resp$LOCATION != "Vanuatu")
 dose_resp$LOCATION <- as.factor(dose_resp$LOCATION)
 
-exclude <- c("MEAN_RATE_NEO", "MEAN_RATE_NEO_U5", "PUBLIC_EXP_PER_CAP_LAGGED2", "HEALTH_EXP_LAGGED2", "OTHER_EXP_LAGGED2", "LOCATION")
-
-corr_publ <- cor(x = dose_resp[,!(colnames(dose_resp) %in% exclude)], y = dose_resp$PUBLIC_EXP_PER_CAP_LAGGED2) %>%
-	as.data.frame()
-corr_publ <- subset(corr_publ, corr_publ$V1 >= 0.2 | corr_publ$V1 <= -0.2)
+exclude <- c("MEAN_RATE_NEO", "MEAN_RATE_NEO_U5", "PUBLIC_EXP_PER_CAP_LAGGED", "HEALTH_EXP_LAGGED", "OTHER_EXP_LAGGED", "LOCATION")
 
 
-corr_health <- cor(x = dose_resp[,!(colnames(dose_resp) %in% exclude)], y = dose_resp$HEALTH_EXP_LAGGED2) %>%
-	as.data.frame()
-corr_health <- subset(corr_health, corr_health$V1 >= 0.2 | corr_health$V1 <= -0.2)
 
 
-corr_other <- cor(x = dose_resp[,!(colnames(dose_resp) %in% exclude)], y = dose_resp$OTHER_EXP_LAGGED2) %>%
-	as.data.frame()
-corr_other <- subset(corr_other, corr_other$V1 >= 0.2 | corr_other$V1 <= -0.2)
+
+# Bivariate analysis for match -----------------------
+external_factors <- c("FERTILITY_RATE_LAGGED", "PLUS_65_YEARS_LAGGED", "GDP_PER_CAP_LAGGED", "OOP_PER_CAP_LAGGED", "GINI_LAGGED", 
+			"POVERTY_GAP_LAGGED", "INFLATION_LAGGED", "UNEMPLOYMENT_LAGGED", "BASIC_SANITATION_LAGGED", "BASIC_WATER_LAGGED", 
+			"UNEMPLOYMENT_FEM_LAGGED", "SCHOOL_FEM_LAGGED", "WOMEN_PARLIAMENT_LAGGED", "SCHOOL_LIFE_EXP_LAGGED", "OUT_OF_SCHOOL_LAGGED", 
+			"CONTROL_CORRUPTION_LAGGED", "GOV_EFFECTIVENESS_LAGGED", "POLITICAL_STABILITY_LAGGED", "REGULATORY_QUALITY_LAGGED", "RULE_OF_LAW_LAGGED", 
+			"UNDERNOURISHMENT_LAGGED", "DOCTORS_LAGGED", "DELIVERY_ASSISTANCE_LAGGED", "AIDS_PREVALENCE_LAGGED", "MALARIA_INCIDENCE_LAGGED", 
+			"DPT_LAGGED", "HOSPITAL_BEDS_LAGGED", "ELECTRICITY_LAGGED", "URBAN_RATE_LAGGED", "pop", "POP_DENS", "LONG", "LAT")
+
+p_cor_ext <- function(compare){ #function to calculate p value of pearson correlationg and extract external factors with p <= 0.25
+	biv_compare <-list()
+	for(i in 1:length(external_factors)){
+		biv_compare[[i]] <- cor.test(x = as.matrix(dose_resp[,colnames(dose_resp) == compare]), 
+					       y = as.matrix(dose_resp[,colnames(dose_resp) == external_factors[i]]))$p.value
+	}	
+	biv_compare <- do.call(cbind, biv_compare)
+	biv_compare <- t(biv_compare) %>% as.data.frame()
+	names(biv_compare) <- "p_value"
+	biv_compare$ext_factor <- external_factors
+	biv_compare <- subset(biv_compare, biv_compare$p_value <= 0.25)
+	biv_compare <- biv_compare$ext_factor
+	a <- paste(head(biv_compare,-1), " + ")
+	a <- c(paste(compare," ~"), a, tail(biv_compare,1))
+	return(a)
+}
+
+
+p_cor_public <- p_cor_ext("PUBLIC_EXP_PER_CAP_LAGGED")
+p_cor_health <- p_cor_ext("HEALTH_EXP_LAGGED")
+p_cor_other <- p_cor_ext("OTHER_EXP_LAGGED")
+
+
 
 # Match -------------------------------------------------------------------
 #SuperLearner::listWrappers()
-
 #Public Expenditure
 set.seed(123)
-m.out_public <- weightit(PUBLIC_EXP_PER_CAP_LAGGED2 ~ 
-			GDP_PER_CAP_LAGGED1 +
-			OOP_PER_CAP_LAGGED3 +
-			POLITICAL_STABILITY_LAGGED,
+m.out_public <- weightit(formula = as.formula(p_cor_public),
 		  		data= dose_resp, method = "super",  				
-		 		SL.library = c("SL.nnls"))
+		 		SL.library = c("SL.bayesglm",
+					       "SL.gbm",
+					       "SL.glm",
+					       "SL.knn",
+					       "SL.lda",
+					       "SL.nnet",
+					       "SL.nnls",
+					       "SL.ranger",
+					       "SL.xgboost"
+					       ))
 summary(m.out_public)
-
 bal.tab(m.out_public, un = TRUE, disp.v.ratio = TRUE, m.threshold = .05,continuous = "std")
-
-
-library("survey")
 d.w_public <- svydesign(ids = ~1, 
 		 weights = m.out_public$weights, data = dose_resp)
-fit_public <- svyglm(MEAN_RATE_NEO ~ PUBLIC_EXP_PER_CAP_LAGGED2  +
-	   	          	FERTILITY_RATE_LAGGED +          
-				PLUS_65_YEARS_LAGGED +          
-				GDP_PER_CAP_LAGGED1 +
-				OOP_PER_CAP_LAGGED3 +
-				GINI_LAGGED + 
-				POVERTY_GAP_LAGGED +            
-				INFLATION_LAGGED + 
-				UNEMPLOYMENT_LAGGED +
-				BASIC_SANITATION_LAGGED +       
-				BASIC_WATER_LAGGED +  
-				UNEMPLOYMENT_FEM_LAGGED + 
-				SCHOOL_FEM_LAGGED +             
-				WOMEN_PARLIAMENT_LAGGED +  
-				SCHOOL_LIFE_EXP_LAGGED +  
-				OUT_OF_SCHOOL_LAGGED +          
-				CONTROL_CORRUPTION_LAGGED +  
-				GOV_EFFECTIVENESS_LAGGED + 
-				POLITICAL_STABILITY_LAGGED +    
-				REGULATORY_QUALITY_LAGGED +  
-				RULE_OF_LAW_LAGGED +            
-				UNDERNOURISHMENT_LAGGED +       
-				DOCTORS_LAGGED +          
-				DELIVERY_ASSISTANCE_LAGGED +  
-				AIDS_PREVALENCE_LAGGED +        
-				MALARIA_INCIDENCE_LAGGED +    
-				DPT_LAGGED +                   
-				HOSPITAL_BEDS_LAGGED +          
-				ELECTRICITY_LAGGED +       
-				URBAN_RATE_LAGGED +       
-				SURFACE +                       
-				POP_DENS +               
-				LONG +                          
-				LAT, design = d.w_public)
-coef(fit_public)
-summary(fit_public)
-summ(fit_public, confint = TRUE, 
-model.fit_public = FALSE, model.info = FALSE, digits = 7) 
+
 
 #Health Expenditure
 set.seed(123)
-m.out_health <- weightit(HEALTH_EXP_LAGGED2 ~ 
-			GDP_PER_CAP_LAGGED1 +        
-			OOP_PER_CAP_LAGGED3 +        
-			BASIC_SANITATION_LAGGED +    
-			SCHOOL_FEM_LAGGED +         
-			SCHOOL_LIFE_EXP_LAGGED +     
-			CONTROL_CORRUPTION_LAGGED +  
-			GOV_EFFECTIVENESS_LAGGED +  
-			POLITICAL_STABILITY_LAGGED + 
-			REGULATORY_QUALITY_LAGGED + 
-			RULE_OF_LAW_LAGGED +        
-			URBAN_RATE_LAGGED +         
-			LAT,
-		  	data= dose_resp, method = "super",  				
-		 	SL.library = c("SL.nnls"))
+m.out_health <- weightit(formula = as.formula(p_cor_health),
+		  		data= dose_resp, method = "super",  				
+		 		SL.library = c("SL.bayesglm",
+					       "SL.gbm",
+					       "SL.glm",
+					       "SL.knn",
+					       "SL.lda",
+					       "SL.nnet",
+					       "SL.nnls",
+					       "SL.ranger",
+					       "SL.xgboost"
+					       ))
 summary(m.out_health)
-
 bal.tab(m.out_health, un = TRUE, disp.v.ratio = TRUE, m.threshold = .05,continuous = "std")
-
-
-
 d.w_health <- svydesign(ids = ~1, 
 		 weights = m.out_health$weights, data = dose_resp)
-fit_health <- svyglm(MEAN_RATE_NEO ~ HEALTH_EXP_LAGGED2  +
-	   	          	FERTILITY_RATE_LAGGED +          
-				PLUS_65_YEARS_LAGGED +          
-				GDP_PER_CAP_LAGGED1 +
-				OOP_PER_CAP_LAGGED3 +
-				GINI_LAGGED + 
-				POVERTY_GAP_LAGGED +            
-				INFLATION_LAGGED + 
-				UNEMPLOYMENT_LAGGED +
-				BASIC_SANITATION_LAGGED +       
-				BASIC_WATER_LAGGED +  
-				UNEMPLOYMENT_FEM_LAGGED + 
-				SCHOOL_FEM_LAGGED +             
-				WOMEN_PARLIAMENT_LAGGED +  
-				SCHOOL_LIFE_EXP_LAGGED +  
-				OUT_OF_SCHOOL_LAGGED +          
-				CONTROL_CORRUPTION_LAGGED +  
-				GOV_EFFECTIVENESS_LAGGED + 
-				POLITICAL_STABILITY_LAGGED +    
-				REGULATORY_QUALITY_LAGGED +  
-				RULE_OF_LAW_LAGGED +            
-				UNDERNOURISHMENT_LAGGED +       
-				DOCTORS_LAGGED +          
-				DELIVERY_ASSISTANCE_LAGGED +  
-				AIDS_PREVALENCE_LAGGED +        
-				MALARIA_INCIDENCE_LAGGED +    
-				DPT_LAGGED +                   
-				HOSPITAL_BEDS_LAGGED +          
-				ELECTRICITY_LAGGED +       
-				URBAN_RATE_LAGGED +       
-				SURFACE +                       
-				POP_DENS +               
-				LONG +                          
-				LAT, design = d.w_health)
-coef(fit_health)
-summary(fit_health)
-summ(fit_health, confint = TRUE, 
-model.fit_health = FALSE, model.info = FALSE, digits = 7) 
 
-#Health Other
+
+#Other Expenditure
 set.seed(123)
-m.out_other <- weightit(OTHER_EXP_LAGGED2 ~ 
-			GDP_PER_CAP_LAGGED1 +
-			OOP_PER_CAP_LAGGED3 +
-			POLITICAL_STABILITY_LAGGED,
-		  	data= dose_resp, method = "super",  				
-		 	SL.library = c("SL.nnls"))
+m.out_other <- weightit(formula = as.formula(p_cor_other),
+		  		data= dose_resp, method = "super",  				
+		 		SL.library = c("SL.bayesglm",
+					       "SL.gbm",
+					       "SL.glm",
+					       "SL.knn",
+					       "SL.lda",
+					       "SL.nnet",
+					       "SL.nnls",
+					       "SL.ranger",
+					       "SL.xgboost"
+					       ))
 summary(m.out_other)
-
 bal.tab(m.out_other, un = TRUE, disp.v.ratio = TRUE, m.threshold = .05,continuous = "std")
-
-
-
 d.w_other <- svydesign(ids = ~1, 
 		 weights = m.out_other$weights, data = dose_resp)
-fit_other <- svyglm(MEAN_RATE_NEO ~ OTHER_EXP_LAGGED2  +
-	   	          	FERTILITY_RATE_LAGGED +          
-				PLUS_65_YEARS_LAGGED +          
-				GDP_PER_CAP_LAGGED1 +
-				OOP_PER_CAP_LAGGED3 +
-				GINI_LAGGED + 
-				POVERTY_GAP_LAGGED +            
-				INFLATION_LAGGED + 
-				UNEMPLOYMENT_LAGGED +
-				BASIC_SANITATION_LAGGED +       
-				BASIC_WATER_LAGGED +  
-				UNEMPLOYMENT_FEM_LAGGED + 
-				SCHOOL_FEM_LAGGED +             
-				WOMEN_PARLIAMENT_LAGGED +  
-				SCHOOL_LIFE_EXP_LAGGED +  
-				OUT_OF_SCHOOL_LAGGED +          
-				CONTROL_CORRUPTION_LAGGED +  
-				GOV_EFFECTIVENESS_LAGGED + 
-				POLITICAL_STABILITY_LAGGED +    
-				REGULATORY_QUALITY_LAGGED +  
-				RULE_OF_LAW_LAGGED +            
-				UNDERNOURISHMENT_LAGGED +       
-				DOCTORS_LAGGED +          
-				DELIVERY_ASSISTANCE_LAGGED +  
-				AIDS_PREVALENCE_LAGGED +        
-				MALARIA_INCIDENCE_LAGGED +    
-				DPT_LAGGED +                   
-				HOSPITAL_BEDS_LAGGED +          
-				ELECTRICITY_LAGGED +       
-				URBAN_RATE_LAGGED +       
-				SURFACE +                       
-				POP_DENS +               
-				LONG +                          
-				LAT, design = d.w_other)
-coef(fit_other)
-summary(fit_other)
-summ(fit_other, confint = TRUE, 
-model.fit_other = FALSE, model.info = FALSE, digits = 7) 
+
+
+# Bivariate analysis for regression -----------------------
+
+p_cor_ext_reg <- function(compare, treatment){ #function to calculate p value of pearson correlationg and extract external factors with p <= 0.25
+	biv_compare <-list()
+	for(i in 1:length(external_factors)){
+		biv_compare[[i]] <- cor.test(x = as.matrix(dose_resp[,colnames(dose_resp) == compare]), 
+					       y = as.matrix(dose_resp[,colnames(dose_resp) == external_factors[i]]))$p.value
+	}	
+	biv_compare <- do.call(cbind, biv_compare)
+	biv_compare <- t(biv_compare) %>% as.data.frame()
+	names(biv_compare) <- "p_value"
+	biv_compare$ext_factor <- external_factors
+	biv_compare <- subset(biv_compare, biv_compare$p_value <= 0.25)
+	biv_compare <- biv_compare$ext_factor
+	a <- paste(head(biv_compare,-1), " + ")
+	a <- c(paste(compare," ~"),paste(treatment, " + "), a, tail(biv_compare,1))
+	return(a)
+}
+
+
+p_cor_public_neo <- p_cor_ext_reg("MEAN_RATE_NEO", "PUBLIC_EXP_PER_CAP_LAGGED")
+p_cor_health_neo <- p_cor_ext_reg("MEAN_RATE_NEO", "HEALTH_EXP_LAGGED")
+p_cor_other_neo <- p_cor_ext_reg("MEAN_RATE_NEO", "OTHER_EXP_LAGGED")
+
+p_cor_public_neo_u5 <- p_cor_ext_reg("MEAN_RATE_NEO_U5", "PUBLIC_EXP_PER_CAP_LAGGED")
+p_cor_health_neo_u5 <- p_cor_ext_reg("MEAN_RATE_NEO_U5", "HEALTH_EXP_LAGGED")
+p_cor_other_neo_u5 <- p_cor_ext_reg("MEAN_RATE_NEO_U5", "OTHER_EXP_LAGGED")
+
+
+
+# Regression --------------------------------------------------------------
+#Neo - Public
+fit_public_neo <- svyglm(as.formula(p_cor_public_neo), design = d.w_public)
+coef(fit_public_neo)
+summary(fit_public_neo)
+summ(fit_public_neo, confint = TRUE,
+model.fit_public_neo = FALSE, model.info = FALSE, digits = 7) 
+
+#Neo - Health
+fit_health_neo <- svyglm(as.formula(p_cor_health_neo), design = d.w_health)
+coef(fit_health_neo)
+summary(fit_health_neo)
+summ(fit_health_neo, confint = TRUE,
+model.fit_health_neo = FALSE, model.info = FALSE, digits = 7) 
+
+#Neo - Other
+fit_other_neo <- svyglm(as.formula(p_cor_other_neo), design = d.w_other)
+coef(fit_other_neo)
+summary(fit_other_neo)
+summ(fit_other_neo, confint = TRUE,
+model.fit_other_neo = FALSE, model.info = FALSE, digits = 7) 
+
+
+#Neo_u5 - Public
+fit_public_neo_u5 <- svyglm(as.formula(p_cor_public_neo_u5), design = d.w_public)
+coef(fit_public_neo_u5)
+summary(fit_public_neo_u5)
+summ(fit_public_neo_u5, confint = TRUE,
+model.fit_public_neo_u5 = FALSE, model.info = FALSE, digits = 7) 
+
+#Neo_u5 - Health
+fit_health_neo_u5 <- svyglm(as.formula(p_cor_health_neo_u5), design = d.w_health)
+coef(fit_health_neo_u5)
+summary(fit_health_neo_u5)
+summ(fit_health_neo_u5, confint = TRUE,
+model.fit_health_neo_u5 = FALSE, model.info = FALSE, digits = 7) 
+
+#Neo_u5 - Other
+fit_other_neo_u5 <- svyglm(as.formula(p_cor_other_neo_u5), design = d.w_other)
+coef(fit_other_neo_u5)
+summary(fit_other_neo_u5)
+summ(fit_other_neo_u5, confint = TRUE,
+model.fit_other_neo_u5 = FALSE, model.info = FALSE, digits = 7) 
+
+
